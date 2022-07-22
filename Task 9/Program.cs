b12c5749eb80bbd3550555b25b6459b49f334980
Syrym <60620893+oblist7@users.noreply.github.com>
2022-07-23 00:12:19 +0600

    add task 9
    Ping url
    async await

---
+++ Task 9/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ *Task 9) Применение Ping для проверки сайта работаетли
+ * 
+ * async await
+ * 
+ */
+
+using System.Net.NetworkInformation;
+
+string url = "https://google.com";
+
+await IsWebSiteAlive(url);
+
+async Task<bool> IsWebSiteAlive(string url)
+{
+    Ping ping = new Ping();
+    string host = new Uri(url).Host;
+
+    PingReply result = await ping.SendPingAsync(host);
+    return result.Status == IPStatus.Success;
+}
