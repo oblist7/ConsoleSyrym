c06bd422fd02766b83a323c5deecf457273ded77
Syrym <60620893+oblist7@users.noreply.github.com>
2022-09-17 22:30:41 +0600

    Generic Classes

---
+++ Task 24/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Generic Classes
+ * 
+ * The class Elems creates a 3-sized array of integers, defines Add() and Show() methods
+ * to store the elements into the array, and shows them separated by a space.
+ * Modify the class to make it generic to execute the same actions with string type, given in the Main function.
+ */
+
+namespace SoloLearn;
+
+class Elems<T>
+{
+    public T[] elements = new T[3];
+
+    public void Add(T elem1, T elem2, T elem3)
+    {
+        elements[0] = elem1;
+        elements[1] = elem2;
+        elements[2] = elem3;
+    }
+
+    public void Show()
+    {
+        foreach (T item in elements)
+        {
+            Console.Write(item + " ");
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Elems<string> elems1 = new Elems<string>();
+
+        elems1.Add("John", "Tamara", "David");
+        elems1.Show();
+
+        Console.WriteLine();
+
+        Elems<int> elems2 = new Elems<int>();
+
+        elems2.Add(5, 14, 13);
+        elems2.Show();
+    }
+}
+
